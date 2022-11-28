a26851feab6e544ca881995cad64b97b2994a616
Kishor Mahmud <dev.kishor138@gmail.com>
2022-11-28 17:07:12 +0600

    update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 const getAllNews = () => {
     const url = 'https://openapi.programming-hero.com/api/news/category/08';
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayAllNews(data.data));
+        .then(res => res.json())
+        .then(data => displayAllNews(data.data));
 }
 
-const displayAllNews =  allNews => {
+const displayAllNews = allNews => {
     const newsContainer = document.querySelector('.news-section');
     allNews.forEach(news => {
         // console.log(news);
@@ -14,7 +14,7 @@ const displayAllNews =  allNews => {
         div.classList = 'news';
         div.innerHTML = `
         <div class="img-box">
-            <img src="${news.image_url}" alt="">
+            <img src="${news.thumbnail_url}" alt="">
         </div>
         <div class="textbox">
             <div class="text">
@@ -43,45 +43,59 @@ const displayAllNews =  allNews => {
                     <i class="fa-regular fa-star"></i>
                 </div>
                 <div class="read-more">
-                    <button>Read More <i class="fa-solid fa-arrow-right"></i></i></button>
+                    <button onclick="getDetails('${news._id}')">Read More <i class="fa-solid fa-arrow-right"></i></i></button>
                 </div>
             </div>
         </div>
         `;
         newsContainer.appendChild(div);
     });
-   
+
+}
+
+const getDetails = news_id => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data[0]))
+}
+const displayDetails = data => {
+    // console.log(data);
+    const newsSection = document.querySelector('.news-section');
+    newsSection.innerText = '';
+    const detailsContainer = document.getElementById('details');
+    detailsContainer.innerHTML = `
+        <div class="img-box">
+            <img src="${data.image_url}" alt="">
+        </div>
+        <div class="text-box">
+            <h3>${data.title}</h3>
+            <p>${data.details}</p>
+        </div>
+        <div class="author-box">
+            <div class="author-name">
+                <div class="author-img">
+                    <img src="${data.author.img}" alt="">
+                </div>
+                <div class="name">
+                    <h4>${data.author.name}</h4>
+                    <p>${data.author.published_date.slice(0, 11)}</p>
+                </div>
+            </div>
+            <div class="icon-box">
+                <p><i class="fa-regular fa-eye"></i> <span>${data.total_view}</span></p>
+                <p><i class="fa-solid fa-comment"></i> <span>3.5K</span></p>
+                <p><i class="fa-solid fa-share-nodes"></i> <span>3.5K</span></i></p>
+            </div>
+        </div>
+        `;
+
 }
 
 
-// ul.children[1].addEventListener('click', () => {
-//     window.location.href = '../breaking.html';
-// });
-// ul.children[2].addEventListener('click', () => {
-//     window.location.href = '../regular.html';
-// });
-// ul.children[3].addEventListener('click', () => {
-//     window.location.href = '../international.html';
-// });
-// ul.children[4].addEventListener('click', () => {
-//     window.location.href = '../sports.html';
-// });
-// ul.children[5].addEventListener('click', () => {
-//     window.location.href = '../entertainment.html';
-// });
-// ul.children[6].addEventListener('click', () => {
-//     window.location.href = '../culture.html';
-// });
-// ul.children[7].addEventListener('click', () => {
-//     window.location.href = '../arts.html';
-// });
-// ul.children[8].addEventListener('click', () => {
-//     window.location.href = '../all.html';
-// });
 
 
 
 
 getAllNews();
 
-// getCatagory();
